e3740a901f7b41572f9ed05cf2cd2a89066567be
ggomma <ggomma@gmail.com>
2022-07-16 18:15:07 +0900

    feat: implement SBT operation logic

--- SBT.sol
+++ SBT.sol
@@ -5,9 +5,35 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721, ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract SBT is ERC721URIStorage, Ownable {
+    uint256 private _totalIssuedTokenAmount;
+    uint256 private _totalBurntTokenAmount;
+
     constructor(string memory _name, string memory _symbol)
         ERC721(_name, _symbol)
     {}
+
+    function mint(address _receiver, string calldata _tokenURI) external {
+        _safeMint(_receiver, _totalIssuedTokenAmount);
+        _setTokenURI(_totalIssuedTokenAmount, _tokenURI);
+        _totalIssuedTokenAmount += 1;
+    }
+
+    function burn(uint256 _tokenId) external {
+        require(
+            _isApprovedOrOwner(_msgSender(), _tokenId),
+            "Caller is not token owner nor approved"
+        );
+        _burn(_tokenId);
+        _totalBurntTokenAmount += 1;
+    }
+
+    function totalIssuedTokens() public view returns (uint256) {
+        return _totalIssuedTokenAmount;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalIssuedTokenAmount + _totalBurntTokenAmount;
+    }
 
     function approve(address, uint256) public pure override {
         revert("Can not approve SBT");
